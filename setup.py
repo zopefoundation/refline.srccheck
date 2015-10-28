08cb4d1b2c19b06af6526a2acac5b87720f3996b
Adam Groszer <agroszer@gmail.com>
2015-10-28 14:57:44 +0100

    Back to development: 0.8.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name="refline.srccheck",
-    version='0.8.0',
+    version='0.8.1.dev0',
     url="http://pypi.python.org/pypi/refline.srccheck/",
     install_requires=[
         'setuptools',
